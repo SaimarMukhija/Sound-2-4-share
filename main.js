3580e4dac876baba89c2220c0881478d6fdc432d
SaimarMukhija <93813780+SaimarMukhija@users.noreply.github.com>
2022-08-08 16:54:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ function gotresults(error , results ) {
         random_number_g = Math.floor(Math.random() * 255) + 1;
         random_number_b = Math.floor(Math.random() * 255) + 1;
 
-        document.getElementById("result_label").innerHTML = "I can hear -----"
+        document.getElementById("result_label").innerHTML = "I can hear -----"+
         results[0].label;
-        document.getElementById("result_confidence").innerHTML = "Accuracy -----"
+        document.getElementById("result_confidence").innerHTML = "Accuracy -----"+
         (results[0].confidence*100).toFixed(2)+"%";
         document.getElementById("result_label").style.color = "rgb("
         +random_number_r+","+random_number_g+","+random_number_r+")";
